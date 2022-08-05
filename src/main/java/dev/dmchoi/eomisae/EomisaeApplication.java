f1b2eee92fccfdf925ec91d2104c0c89735914e1
Dongmin <dmchoi224@naver.com>
2022-08-05 13:52:25 +0900

    First COMMIT!

---
+++ src/main/java/dev/dmchoi/eomisae/EomisaeApplication.java
@@ -0,0 +1,13 @@
+package dev.dmchoi.eomisae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EomisaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EomisaeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/dmchoi/eomisae/controllers/RootController.java
@@ -0,0 +1,16 @@
+package dev.dmchoi.eomisae.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller(value = "dev.dmchoi.eomisae.controllers.RootController")
+@RequestMapping(value = "/")
+public class RootController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getIndex(ModelAndView modelAndView) {
+        modelAndView.setViewName("root/index");
+        return modelAndView;
+    }
+}
