fa4d34b02671a6dc2b27f13031502cdb230f5187
zirrcon <ttysw0628@naver.com>
2022-08-05 14:53:39 +0900

    Test Commit

---
+++ src/test/java/dev/dmchoi/eomisae/EomisaeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dmchoi.eomisae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EomisaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
