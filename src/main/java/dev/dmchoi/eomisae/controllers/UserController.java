96c779e27d511108d0c1ca364b2f1b6b3af108e9
zirrcon <ttysw0628@naver.com>
2022-08-10 17:02:16 +0900

    my-page commit

--- src/main/java/dev/dmchoi/eomisae/controllers/UserController.java
+++ src/main/java/dev/dmchoi/eomisae/controllers/UserController.java
@@ -13,4 +13,22 @@ public class UserController {
         modelAndView.setViewName("user/memberSignUpForm");
         return modelAndView;
     }
+
+    @RequestMapping(value = "/my-page/memberInfo", method = RequestMethod.GET)
+    public ModelAndView getMemberInfo(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberInfo");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberScrappedDocument", method = RequestMethod.GET)
+    public ModelAndView getMemberScrappedDocument(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberScrappedDocument");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberSavedDocument", method = RequestMethod.GET)
+    public ModelAndView getMemberSavedDocument(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberSavedDocument");
+        return modelAndView;
+    }
 }
