66018a6006ec30ac1bd667a47c52e2b92ed30360
zirrcon <ttysw0628@naver.com>
2022-08-12 17:02:22 +0900

    my-page commit

--- src/main/java/dev/dmchoi/eomisae/controllers/UserController.java
+++ src/main/java/dev/dmchoi/eomisae/controllers/UserController.java
@@ -31,4 +31,76 @@ public class UserController {
         modelAndView.setViewName("user/my-page/memberSavedDocument");
         return modelAndView;
     }
+
+    @RequestMapping(value = "/my-page/memberOwnDocument", method = RequestMethod.GET)
+    public ModelAndView getMemberOwnDocument(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberOwnDocument");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberOwnVote", method = RequestMethod.GET)
+    public ModelAndView getMemberOwnVote(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberOwnVote");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberCommunicationMessages", method = RequestMethod.GET)
+    public ModelAndView getMemberCommunicationMessages(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberCommunicationMessages");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberLoginlogHistories", method = RequestMethod.GET)
+    public ModelAndView getMemberLoginlogHistories(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberLoginlogHistories");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberPointHistoryList", method = RequestMethod.GET)
+    public ModelAndView getMemberPointHistoryList(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberPointHistoryList");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberCashSendLog", method = RequestMethod.GET)
+    public ModelAndView getMemberCashSendLog(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberCashSendLog");
+        return modelAndView;
+    }
+    @RequestMapping(value = "/my-page/memberCashHistoryList", method = RequestMethod.GET)
+    public ModelAndView getMemberCashHistoryList(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberCashHistoryList");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberOwnComment", method = RequestMethod.GET)
+    public ModelAndView getMemberOwnComment(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberOwnComment");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberOwnVotePosting", method = RequestMethod.GET)
+    public ModelAndView getMemberOwnVotePosting(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberOwnVotePosting");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberOwnBlockList", method = RequestMethod.GET)
+    public ModelAndView getMemberOwnBlockList(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberOwnBlockList");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberSnsManage", method = RequestMethod.GET)
+    public ModelAndView getMemberSnsManage(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberSnsManage");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/my-page/memberAutoLoginManager", method = RequestMethod.GET)
+    public ModelAndView getMemberAutoLoginManager(ModelAndView modelAndView) {
+        modelAndView.setViewName("user/my-page/memberAutoLoginManager");
+        return modelAndView;
+    }
+
 }
